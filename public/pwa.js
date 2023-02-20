b15055db5472e34282987147d9830623717471c6
Simon Ugorji <ugorji757@gmail.com>
2023-02-20 19:36:38 +0100

    Modified pwa.js script file

--- public/pwa.js
+++ public/pwa.js
@@ -6,19 +6,19 @@ window.addEventListener('beforeinstallprompt', (e) => {
 
 window.addEventListener('load', () => {
     const pwaAppInstallBtn = document.querySelectorAll('.pwaAppInstallBtn');
-    console.log(pwaAppInstallBtn)
     pwaAppInstallBtn.forEach(el => {
-        el.addEventListener('click', async () => {
-            if (deferredPrompt !== null) {
+        if (deferredPrompt === null) {
+            //hide the button
+            el.style.display = "none";
+        } else {
+            el.addEventListener('click', async () => {
                 deferredPrompt.prompt();
                 const { outcome } = await deferredPrompt.userChoice;
                 if (outcome === 'accepted') {
                     deferredPrompt = null;
                 }
-            }else{
-                console.log("deferred prompt is null [Website cannot be installed]")
-            }
-        });
+            });
+        }
     })
 })
 
